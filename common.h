6d452bb028faea5564c4f5876cb46bf4da08e8db
Adil Ahmad <adilahmad17@gmail.com>
2024-02-27 23:57:26 -0700

    update common.h

--- common.h
+++ common.h
@@ -8,5 +8,17 @@ struct free_info{
 	unsigned long vaddr;
 };
 
+struct sharepage_info{
+	unsigned long vaddr;
+	int 		  client_pid;
+	bool		  write;		// permission-related
+};
+
+struct accesspage_info{
+	unsigned long vaddr;
+};
+
 #define ALLOCATE		_IOW('a', 'a', struct alloc_info *)
 #define FREE			_IOW('a', 'b', struct free_info *)
+#define SHAREPAGE		_IOW('a', 'c', struct sharepage_info *)
+#define ACCESSPAGE		_IOW('a', 'd', struct accesspage_info *)
